2e34936f2fb3ec16e730edb7ec35ef242c44dcd0
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-11-10 10:07:53 -0600

    Exploring datatypes and string manipulation

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ app = Flask(__name__)
 def hello_world():
     split_string = "This string has been\nsplit\nover\nseveral\nlines"
     print(split_string)
+    tabbedString = "1\t2\t3\t4\t5"
+    name = input("Please enter name")
     print("Hello, everyone")
-    return split_string
+    print(type(name))
+    print(name + " is " + " hello ")
+    print(tabbedString)
+    return "Hello", name
 
 
+#Practice Scratches
+coolRunning = "A cool jamaican movie"
+print(coolRunning[2], coolRunning[4], coolRunning[7], coolRunning[9])
 if __name__ == '__main__':
     app.run()
